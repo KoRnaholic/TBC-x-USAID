a74638a3847b369b105832d0450e3f85616eb333
KoRnaholic <a_arnoup@cu.edu.ge>
2024-01-22 01:57:00 +0400

    added responsive design

--- src/script.js
+++ src/script.js
@@ -1,7 +1,16 @@
+
+// displayng menu 
 const btnEl = document.querySelector('.btn-mobile-nav')
 const headerEl = document.querySelector('.nav-list')
 
 btnEl.addEventListener('click', function () {
     headerEl.classList.add('navbar-open');
+
+})
+
+// removing menu 
+const btnCloseEl = document.querySelector('.btn-close-nav')
+btnCloseEl.addEventListener('click', function () {
+    headerEl.classList.remove('navbar-open');
 
 })

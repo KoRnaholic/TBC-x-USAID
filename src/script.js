bd5e47898d7c56c4233e8d74777f9d37e6c1586e
KoRnaholic <a_arnoup@cu.edu.ge>
2024-01-22 20:29:05 +0400

    added more css styles

--- src/script.js
+++ src/script.js
@@ -14,3 +14,14 @@ btnCloseEl.addEventListener('click', function () {
     headerEl.classList.remove('navbar-open');
 
 })
+
+//transparent nav
+const navEl = document.querySelector('.nav-container')
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 50) {
+        navEl.classList.add('nav-transparent')
+    } else {
+        navEl.classList.remove('nav-transparent')
+    }
+})
